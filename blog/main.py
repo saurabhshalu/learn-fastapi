98f74611258f93ae2d7e97cf45430a20f789d10e
Saurabh Verma <saurabhshalu@gmail.com>
2023-10-05 15:36:26 +0530

    Database initial setup

---
+++ blog/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from . import schemas
+
+app = FastAPI()
+
+
+@app.post('/blog')
+def create(request: schemas.Blog):
+    return {'title': request.title, 'body': request.body}
